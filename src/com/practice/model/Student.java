4a4828d23bbbd1e010e23e607866ff122392d729
nareshvaddi <nareshvaddi88@gmail.com>
2021-10-28 09:40:07 +0530

    Adding new file

---
+++ src/com/practice/model/Student.java
@@ -0,0 +1,29 @@
+package com.practice.model;
+
+public class Student {
+
+	private int stdId;
+	private String fname;
+	private String lname;
+	
+	public int getStdId() {
+		return stdId;
+	}
+	public void setStdId(int stdId) {
+		this.stdId = stdId;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	
+}
