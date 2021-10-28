d741005fcae639fd4a480af7393571a65981b7c9
nareshvaddi <nareshvaddi88@gmail.com>
2021-10-28 09:52:36 +0530

    Adding file

---
+++ src/com/practice/model/Product.java
@@ -0,0 +1,22 @@
+package com.practice.model;
+
+public class Product {
+
+	private int prodId;
+	private String name;
+	
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
